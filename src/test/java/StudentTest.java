2d889c7b5507131db45aefbabfa5ac806521be39
Anna <annasereno27@gmail.com>
2023-11-21 11:05:24 -0600

    added test for grades

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -9,8 +9,13 @@ public class StudentTest {
     private Student mickey = null;
 
     @Before//resets student before every single test
-    public void setup(){
+    public void setup() {
         mickey = new Student(1, "Mickey");
+
+        mickey.addGrade(80);
+        mickey.addGrade(89);
+        mickey.addGrade(90);
+        mickey.addGrade(78);
     }
 
     @Test
@@ -25,6 +30,24 @@ public class StudentTest {
         assertEquals("Mickey", mickey.getName());
     }
 
+    @Test
+    public void testEquals(){
+        Student otherMickey = new Student(1, "Mickey");
+        assertEquals(mickey, otherMickey);
+    }
 
+    @Test
+    public void testGradeAverage(){
+        assertEquals(84.25, mickey.getGradeAverage(), 0.01);
+    }
+
+    @Test
+    public void testAddGrade(){
+        //add a single grade to see if added to arrayList
+        mickey.getGrades().clear();
+        mickey.addGrade(78);
+        assertEquals(1, mickey.getGrades().size());
+        assertEquals(78, mickey.getGrades().get(0).intValue());
+    }
 
 }
